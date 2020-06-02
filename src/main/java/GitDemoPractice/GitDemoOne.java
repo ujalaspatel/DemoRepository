8525c4ff911d3f0fefc899fc31dd87bb1384c879
ujalaspatel <66299793+ujalaspatel@users.noreply.github.com>
2020-06-02 07:37:39 -0400

    Update GitDemoOne.java

--- src/main/java/GitDemoPractice/GitDemoOne.java
+++ src/main/java/GitDemoPractice/GitDemoOne.java
@@ -3,7 +3,7 @@ package GitDemoPractice;
 public class GitDemoOne {
 
 	public static void main(String[] args) {
-		System.out.println("GitDemoOne");
+		System.out.println("GitDemoOne"); //this added comment is for pulling demo
 
 	}
 
